a89b292a746450903722ec634969ee052fc2603a
PUCHACZEWSKIc <c.puchaczewski.pro@gmail.com>
2024-01-28 18:37:05 +0100

    commit

--- align.py
+++ align.py
@@ -1,40 +1,27 @@
 #Ce fichier a pour but d'aligner des séquences sur une séquence consensus (fournit par le logiciel Jalview)
 
-#Séquence consensus : 
-
+#On va utiliser le module parasil, qui nous permet d'aligner nos séquences sur la séquence, et nous fournis la séquence alignée (traceback)
 import parasail
+
+#Séquence consensus : 
 
 seq_cons = ""
 
-with open("Sequence_consensus.txt") as file :
+with open("Sequence_consensus.txt") as file : #Temporaire, ici pour des raisons de test
     for line in file : 
         seq_cons += line
 
 class align : 
-    
+    #Notre classe prends en paramètre la séquence consensus et la liste des séquences que l'on veux aligner dessus.
     def __init__(self, seq_cons, seq_list) : 
         self.__seq_cons = seq_cons
         self.__seq_list = seq_list
     
     def align(self) :
-        cigar_list = []
+        align_list = []
         for seq in self.__seq_list :
-            test = parasail.nw_trace(seq, self.__seq_cons, 10, 1, parasail.dnafull)
-            cigar_list.append(test.cigar.decode)
-            traceback = test.get_traceback()
-            print(traceback)
-        return cigar_list
+            test = parasail.nw_trace_scan(seq, self.__seq_cons, 10, 1, parasail.dnafull) #On utilise parasail pour aligner nos séquences. 
+            traceback = test.get_traceback().query #On récupère la séquence alignée et on la stocke dans une nouvelle liste
+            align_list.append(traceback)
+        return align_list
 
-from Extract_fasta import Fasta_extract 
-import cigar
-
-testaille = Fasta_extract('sequences_SARS-CoV-2_ech.fasta')
-
-data_test = testaille.sequences()
-
-testouille = align(seq_cons, data_test)
-
-test = testouille.align()
-for i in test : 
-    cig = cigar.Cigar(i)
-    print(cig)
