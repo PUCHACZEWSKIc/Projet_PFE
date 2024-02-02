0d353a7d1537f3670e4dd16cf799f9c4a84e2379
PUCHACZEWSKIc <c.puchaczewski.pro@gmail.com>
2024-02-02 16:57:50 +0100

    Ajout d'une nouvelle fonction permettant de séparer les séquences en petits fichiers de X séquences, stockées dans un dossier annexe.

--- Extract_fasta.py
+++ Extract_fasta.py
@@ -1,5 +1,6 @@
 #Ce fichier permet d'extraire les séquences d'un fichier fasta sous forme d'une liste contrenant des string, chaque string étant une séquence.
 #Ce procédé efface les noms des séquences.
+import os
 
 class Fasta_extract : 
     
@@ -21,4 +22,33 @@ class Fasta_extract :
                 else :
                     act_line += seq
         sequence_list.append(act_line) 
+        self.__sequence_list = sequence_list[1:len(sequence_list)]
         return sequence_list[1:len(sequence_list)]
+
+    def sequences_packages(self, pack_size) : #Cette fonction permet de récupérer les séquences et de les dispatcher dans un dossier "sequences_packages" en des fichiers contenant "pack_size" sequences
+        if not os.path.exists("sequences_packages"):
+            os.makedirs("sequences_packages") #On vérifie si le dossier d'acceuil existe, et si non, on le créé
+        nbr_seq = 1
+        nbr_pack = 0
+        while nbr_seq-1 != len(self.__sequence_list) : #Cette boucle permet de créer plusieurs fichiers, tant qu'on a pas atteint la fin de la liste.
+            title = "sequences_packages/pack_" + str(nbr_pack) + ".fasta" #On définit le titre du fichier actuel
+            with open(title, 'a') as fileOut : 
+                while nbr_seq%pack_size != 0 : #Tant qu'on a pas remplis le fichier avec le nombre exact de séquences, on continue
+                    fileOut.write(">seq n°" + str(nbr_seq) + "\n" + self.__sequence_list[nbr_seq-1] + '\n\n')
+                    nbr_seq += 1
+                    if nbr_seq == len(self.__sequence_list) : #Si on tombe sur la fin de seq_list à ce moment là, on arrête tout.
+                        break 
+                with open(title, 'a') as fileOut : #Pour éviter de sauter une séquence lorsque le fichier est plein, on y rajoute la dernière séquence (la dernière séquence a un rapport nbr_seq%pack_size de 0 )
+                    fileOut.write(">seq n°" + str(nbr_seq) + "\n" + self.__sequence_list[nbr_seq-1] + '\n\n') 
+            nbr_seq += 1
+            nbr_pack += 1
+
+file = Fasta_extract('sequences_20240202_4425212.fasta') #Test sur un fichier dont le compte de séquence n'est pas rond (237)
+file.sequences()
+file.sequences_packages(100)
+
+#Pensez à supprimer les fichiers dans le dossier recevant entre les deux test.
+
+file = Fasta_extract('sequences_20240202_4425212.fasta') #Test sur un fichier dont le compte de séquence est rond (200)
+file.sequences()
+file.sequences_packages(100)
